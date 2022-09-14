e4ba47f3aa80720831043d0adf1554334b020973
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-14 01:33:33 +0000

    Hello World program updated in cpp

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Ms Raffin, this is a message from Marcus Wehbi.\n";
+    std::cout << "I want you to know that I appreciate the hard work you put into the class.\n";
+    std::cout << "I am really enjoying programming in this class.\n";
+}
